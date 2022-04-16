8c731d9f43d147a86dfbbd8c4e6adec1c4784647
2damin <leesak2003@naver.com>
2022-04-16 09:07:36 +0000

    save_figure

--- util/tools.py
+++ util/tools.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageDraw
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 def show_img(img_data, text):
     _img_data = img_data * 255
     
-    print(_img_data.shape)
     # 4D -> 2D
     _img_data = np.array(_img_data[0,0], dtype=np.uint8)
     
@@ -16,8 +16,9 @@ def show_img(img_data, text):
     
     #draw text in img
     if text is not None:
-        draw.text((cx,cy), text)
+        draw.text((cx,cy), text, fill="red")
+
+    img_data.show()
+    img_data.save("res.jpg")
     
-    plt.imshow(img_data)
-    plt.show()
     
